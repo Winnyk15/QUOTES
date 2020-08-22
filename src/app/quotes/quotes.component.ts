390ec0421e8573a2b914833eac2b49ce4600a452
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-22 20:28:49 +0300

    generate component quotes

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
