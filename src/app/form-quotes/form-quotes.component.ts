1158cbd9e3f5ee0ec453d4c837467ea3a2d49075
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-23 09:05:18 +0300

    add code to form-quotes file

--- src/app/form-quotes/form-quotes.component.ts
+++ src/app/form-quotes/form-quotes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote/quote';
 
 @Component({
   selector: 'app-form-quotes',
