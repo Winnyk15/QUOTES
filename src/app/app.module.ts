93128a9b5019d9d90191ab3921c2f77e489ecbc8
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-23 08:43:04 +0300

    generate highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailComponent
+    QuotesDetailComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
