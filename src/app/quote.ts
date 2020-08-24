ba89d549882a11e131ce49925ec91f6e021eaae5
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-24 22:40:08 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-    public showDetail:boolean;
-    constructor(public id:number, public text:string, public author:string, public submitName:string, public postDate:Date){
-        this.showDetail=false;
-    }
-}
